63310dd7b2f32b4b03c54bab3e4a5eb78a62b099
PMHStudio <pmhstudio.pmh@gmail.com>
2020-03-01 15:33:41 +0900

    Swal Added

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,44 @@
-function create () {
-  const color = document.getElementById('color').value
-  const top = document.getElementById('top').value
-  const middle = document.getElementById('middle').value
-  const bottom = document.getElementById('bottom').value
-  const image = document.getElementById('image').value
-
-  const auth = getMeta('auth')
-  const body = JSON.stringify({ color, top, middle, bottom, image })
-  if (!top || !middle || !bottom || !color || !image) return alert('모든 필드가 채워져 있어야 합니다')
-
-  fetch('/api/create?auth=' + auth, { headers: { 'Content-Type': 'application/json' }, body, method: 'PUT' })
-    .then((res) => { return res.json() })
-    .then((json) => {
-      if (!json.sucess) alert('처리에 실패햐였습니다\n응답: ' + json.reason)
-      else alert('처리가 완료되었습니다\nhttp://' + window.location.host + '/' + json.eid)
-      window.location.reload()
-    })
-}
+Swal.fire({
+  title: 'Embed Maker',
+  text: "심심해서 만든 디스코드 임베딩 도구",
+  imageUrl: 'https://discordapp.com/assets/94db9c3c1eba8a38a1fcf4f223294185.png',
+  imageAlt: 'Discord',
+  confirmButtonText: '시작하기!',
+  allowEscapeKey: false,
+  allowOutsideClick: false
+}).then(() => {
+  Swal.mixin({
+    input: 'text',
+    confirmButtonText: '다음 <i class="fas fa-arrow-alt-circle-right"></i>',
+    allowEscapeKey: false,
+    allowOutsideClick: false,
+    progressSteps: ['1', '2', '3', '4', '5'],
+    inputValidator: (v) => {
+      return new Promise((resolve) => {
+        if (v.length < 1) resolve('아무 값이라도 적어주세요')
+        else resolve()
+      })
+    }
+  }).queue([
+    '임베드 상단에 표시될\n문구를 정해주세요',
+    '임베드 제목을 입력해주세요',
+    '임베드 내용을 입력해주세요',
+    '임베드 색상을 입력해주세요',
+    '임베드 이미지 링크를 입력해주세요'
+  ]).then((result) => {
+    if (result.value) {
+      const v = result.value
+      const data = getMeta('data').split(';')
+      const body = JSON.stringify({ top: v[0], middle: v[1], bottom: v[2], color: v[3], image: v[4] })
+      fetch('/api/create?auth=' + data[0], { headers: { 'Content-Type': 'application/json' }, body, method: 'PUT' })
+        .then((res) => { return res.json() })
+        .then((json) => {
+          if (!json.sucess) Swal.fire({ title: '처리에 실패햐였습니다', text: json.reason, icon: 'error', allowEscapeKey: false, allowOutsideClick: false, showConfirmButton: false })
+          else Swal.fire({ title: '처리가 완료되었습니다', html:'<input class="form-control" type="text" value="http://' + window.location.host + '/' + json.eid + '">', icon: 'success', allowEscapeKey: false, allowOutsideClick: false, showConfirmButton: false })
+        })
+    }
+  })
+})
 
 function getMeta(metaName) {
   const metas = document.getElementsByTagName('meta')
